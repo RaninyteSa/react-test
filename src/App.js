4cd8ccc0768230219e00f446449487e92db451af
Sandra Rainyte <srainyte@gmail.com>
2022-08-18 12:23:46 +0300

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect } from 'react'
 
-function App() {
+
+const App = () => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000')
+    .then(resp => resp.json())
+    .then(resp => {
+      setPosts(resp)
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="articles">
+      {posts && posts.map(article => {
+        return (
+          <div key={article.id} className='box'>
+            <h3>{article.title}</h3>
+            <div className='image'>
+              <img src={article.image} alt={article.title} />
+            </div>
+            <div className="content">
+              {article.content}
+              </div>
+          </div>
+        )
+      })}
     </div>
   );
 }
